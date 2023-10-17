0ade51e178354861f6be0f6e43093c3bfee7f658
juanjosegr <juanjosegr9@gmail.com>
2023-10-17 19:52:35 +0200

    barcos

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val batalla_naval_obj = BatallaNaval()
+
+    batalla_naval_obj.guardarBatallaNaval()
+    println("******************")
+    batalla_naval_obj.recuperarBatallaNaval()
+
+    batalla_naval_obj.pintarTablero()
+
+
+}
